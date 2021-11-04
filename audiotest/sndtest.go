b159e437eef2c4260db004b59ab4f049504f6dab
__Coder__ <allan.de.souza.melo.rj@gmail.com>
2021-11-03 22:12:29 -0300

    ...

--- audiotest/sndtest.go
+++ audiotest/sndtest.go
@@ -9,12 +9,12 @@ import (
 	"github.com/faiface/beep"
 	"github.com/faiface/beep/mp3"
 	"github.com/faiface/beep/speaker"
+	"github.com/faiface/beep/generators"
 )
 
-func main() {
+func old(){
 	var tmp string
 	fmt.Print("music:")
-	// Taking input from user
 	fmt.Scanln(&tmp)
 
 	f, err := os.Open(tmp)
@@ -36,4 +36,38 @@ func main() {
 	})))
 
 	<-done
+
+}
+
+func test(){
+	var tmp string
+	speaker.Init(beep.SampleRate(48000), 4800)//format.SampleRate, format.SampleRate.N(time.Second/10))
+
+	s, err := generators.SinTone(beep.SampleRate(48000), 1000)
+	if err != nil {
+		panic(err)
+	}
+
+	speaker.Play(s)
+	fmt.Scanln(&tmp)
+	speaker.Clear()
+	s2, err := generators.SinTone(beep.SampleRate(48000), 500)
+	if err != nil {
+		panic(err)
+	}
+	speaker.Play(s2)
+	fmt.Scanln(&tmp)
+	speaker.Clear()
+	s3, err := generators.SinTone(beep.SampleRate(48000), 300)
+	if err != nil {
+		panic(err)
+	}
+	speaker.Play(s3)
+	fmt.Scanln(&tmp)
+	speaker.Clear()
+	//fmt.Print("music:")	
+}
+
+func main() {
+	test()
 }
